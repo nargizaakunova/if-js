f429141512376c7e5651ccdc894168c726b793ff
Nargiza Akunova <nargizaakun@gmail.com>
2023-05-24 16:44:43 +0200

    feat: work with primitive data types

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ console.log(student);
 
 user = student;
 console.log(user); // 'Nargiza'
+
+// 7)
+let test = 1;
+test += 1;
+test += '1'; // '21'
+console.log(test); // '21'
+
+test--; // 20
+console.log(test);
+
+console.log(Boolean(test)); // true
+
