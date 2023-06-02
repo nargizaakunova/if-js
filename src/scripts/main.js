549c8725c5a9fc5a96d02c344c0bf4a66c7f65f5
Nargiza Akunova <nargizaakun@gmail.com>
2023-06-02 16:19:44 +0200

    feat: 'sum' function that returns function, closure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('------------------- Lesson 17 -----------------------');
+
+const sum = (num1) => {
+  return (num2) => {
+    return num1 + num2;
+  };
+};
+
+console.log(sum(2)(3));
+console.log(sum(5)(2));
