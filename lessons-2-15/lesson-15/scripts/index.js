849ed21f89e128bc56b1919659252218201272ce
Nargiza Akunova <nargizaakun@gmail.com>
2023-09-16 22:37:26 +0200

    feat: experiment another way of solution

--- lessons-2-15/lesson-15/scripts/index.js
+++ lessons-2-15/lesson-15/scripts/index.js
@@ -1,68 +1,49 @@
-const inputTextEls = document.querySelectorAll('input[type=text]');
-// CLEAR INPUTS VALUE ON CLICK
-function clearInputTextValue(e) {
-  e.target.value = '';
-  e.target.removeEventListener('click', clearInputTextValue);
-}
-inputTextEls.forEach((inputTextEl) =>
-  inputTextEl.addEventListener('click', clearInputTextValue),
-);
-
 // FORM
 const formEl = document.querySelector('form');
 const fileEl = document.getElementById('file');
 const imageDivEl = document.getElementById('image-div');
+
 async function sendDataToServer(body) {
   const fetchOptions = {
     method: 'POST',
     body,
   };
 
-  const response = await fetch(
-    'https://if-student-api.onrender.com/api/file',
-    fetchOptions,
-  );
+  try {
+    const response = await fetch(
+      'https://if-student-api.onrender.com/api/file',
+      fetchOptions,
+    );
 
-  if (!response.ok) {
-    throw new Error('Network response was not OK');
+    if (!response.ok) {
+      console.log('Response is not okay');
+      return;
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (e) {
+    console.log(e.message);
   }
-  return response.json();
 }
+
 formEl.addEventListener('submit', async (event) => {
   event.preventDefault();
   const body = new FormData(formEl);
-  try {
-    const data = await sendDataToServer(body);
-    console.log(data);
-  } catch (e) {
-    console.log('Oh no!', e.message);
-  }
+  await sendDataToServer(body);
 });
 
-// CLICK ON IMAGE DIV:
 imageDivEl.addEventListener('click', () => {
   fileEl.click();
 });
 
-// RESOLVE URL FOR FILE
-function getFileDataURL(file) {
-  return new Promise((resolve) => {
-    const fileReader = new FileReader();
-
-    fileReader.addEventListener('load', (e) => {
-      resolve(e.target.result);
-    });
-    fileReader.readAsDataURL(file);
-  });
-}
-
-// ADD BACKGROUND IMAGE TO THE DIV:
-fileEl.addEventListener('change', async (e) => {
+fileEl.addEventListener('change', (e) => {
   const file = e.target.files[0];
 
-  if (file) {
-    const url = await getFileDataURL(file);
-    imageDivEl.style.backgroundImage = `url(${url})`;
+  const fileReader = new FileReader();
+  fileReader.addEventListener('load', (e) => {
+    imageDivEl.style.backgroundImage = `url(${e.target.result})`;
     imageDivEl.innerHTML = '';
-  }
+  });
+  fileReader.readAsDataURL(file);
 });
